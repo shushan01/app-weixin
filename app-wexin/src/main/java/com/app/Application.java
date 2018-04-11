ba9cebf74e952e17c77d945fe8b5ed4f5816c49d
yangyijun <1012293987@qq.com>
2018-04-11 21:26:48 +0800

    aaa

--- app-wexin/src/main/java/com/app/Application.java
+++ app-wexin/src/main/java/com/app/Application.java
@@ -12,6 +12,7 @@ import org.springframework.web.filter.CorsFilter;
  */
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
